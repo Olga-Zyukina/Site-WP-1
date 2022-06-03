2cb5f5db1e2fee12dec926bfa403e8a361eb2fee
Olga-Zyukina <bloger_80@mail.ru>
2022-06-03 17:56:28 +0300

    Changed design

---
+++ wp1/wp-content/themes/wp1theme/footer.php
@@ -0,0 +1,12 @@
+    <footer class="footer">
+      <div class="container">
+        <div class="footer__text">
+          Copyright &copy; Kenan Hamidic. All rights reserved.
+        </div>
+      </div>
+    </footer>
+
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <script src="js/main.js"></script>
+  </body>
+</html>
