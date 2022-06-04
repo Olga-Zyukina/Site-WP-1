28b283b016f6f2dc8d473fbfa4e64b01ac5444b9
Olga-Zyukina <bloger_80@mail.ru>
2022-06-04 12:02:47 +0300

    Add back-to-top btn

--- js/main.js
+++ js/main.js
@@ -17,3 +17,31 @@ $(document).ready(function() {
         $(this).toggleClass('close');
     });
 });
+
+(function() {
+    'use strict';
+  
+    function trackScroll() {
+      var scrolled = window.pageYOffset;
+      var coords = document.documentElement.clientHeight;
+  
+      if (scrolled > coords) {
+        goTopBtn.classList.add('back-to-top-show');
+      }
+      if (scrolled < coords) {
+        goTopBtn.classList.remove('back-to-top-show');
+      }
+    }
+  
+    function backToTop() {
+      if (window.pageYOffset > 0) {
+        window.scrollBy(0, -80);
+        setTimeout(backToTop, 0);
+      }
+    }
+  
+    var goTopBtn = document.querySelector('.back-to-top');
+  
+    window.addEventListener('scroll', trackScroll);
+    goTopBtn.addEventListener('click', backToTop);
+  })();
